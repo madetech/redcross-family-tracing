f2a3600a8ddfcce30bf14d5a2a807991caf92b84
Will Gibson <will.gibson@walterego.co.uk>
2020-08-06 17:26:36 +0100

    Refactor some of the content steps with helper method

--- cypress/integration/common/Content.js
+++ cypress/integration/common/Content.js
@@ -1,7 +1,14 @@
-import { Then, When } from "cypress-cucumber-preprocessor/steps";
-import { elementNameToClass } from "./support/elementNameToClass";
-import { pageNameToPath } from "./support/pageNameToPath";
-import { textToValue } from "./support/textToValue";
+import {Then, When} from "cypress-cucumber-preprocessor/steps";
+import {elementNameToClass} from "./support/elementNameToClass";
+import {pageNameToPath} from "./support/pageNameToPath";
+import {textToValue} from "./support/textToValue";
+
+function assertElementWithTextLinkingToPageExists(cssSelector, text, pageName) {
+    const element = cy.get(cssSelector);
+    element.contains(text, {matchCase: false});
+    element.should('have.attr', 'href')
+        .and('equal', pageNameToPath[pageName]);
+}
 
 Then(
     /^I see "([^"]*)" in the page heading$/,
@@ -13,35 +20,37 @@ Then(
 Then(
     /^I see a "([^"]*)" button linking to the "([^"]*)" page in the "([^"]*)" element$/,
     function (buttonText, pageName, containingElement) {
-        const button = cy.get(`${elementNameToClass[containingElement]} .redcross-button`);
-        button.contains(buttonText, { matchCase: false });
-        button.should('have.attr', 'href')
-            .and('equal', pageNameToPath[pageName]);
+        assertElementWithTextLinkingToPageExists(
+            `${elementNameToClass[containingElement]} .redcross-button`,
+            buttonText,
+            pageName
+        );
     }
 );
 
 Then(/^I see a "([^"]*)" link linking to the "([^"]*)" page$/,
     function (linkText, pageName) {
-        const link = cy.get('.redcross-back-link');
-        link.contains(linkText, { matchCase: false });
-        link.should('have.attr', 'href')
-            .and('equal', pageNameToPath[pageName]);
+        assertElementWithTextLinkingToPageExists(
+            '.redcross-back-link',
+            buttonText,
+            pageName
+        );
     }
 );
 
 When(
     /^I see a "([^"]*)" button linking to the "([^"]*)" page$/,
     function (buttonText, pageName) {
-        const button = cy.get('.redcross-button');
-        button.contains(buttonText, { matchCase: false });
-        button.should('have.attr', 'href')
-            .and('equal', pageNameToPath[pageName]);
+        assertElementWithTextLinkingToPageExists(
+            '.redcross-button',
+            buttonText,
+            pageName
+        );
     }
 );
 
 When(/^I select "([^"]*)" radio button$/,
     function(radioText){
-        const radioButton = cy.get('[type="radio"]');
-        radioButton.check(textToValue[radioText]);
+        cy.get('[type="radio"]').check(textToValue[radioText]);
     }
 );
